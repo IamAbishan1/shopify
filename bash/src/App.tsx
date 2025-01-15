1376cd2c37d2516af1f3da85d4a44e8e6d0262be
IamAbishan1 <abishanchemjong@gmail.com>
2025-01-15 23:43:58 +0900

    chore : combo box

--- bash/src/App.tsx
+++ bash/src/App.tsx
@@ -2,6 +2,7 @@ import { Navbar } from "./components/navbar"
 // import { AppSidebar } from "./components/sidebar"
 import {  Routes, Route, BrowserRouter as Router } from "react-router-dom"
 import { AppSidebar } from "./components/sidebar"
+import { ComboboxPopover } from "./components/combobox"
 // import { AppSidebar } from "./components/sidebar"
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Navbar/>
       <div className="flex">
     <AppSidebar/>
+    <ComboboxPopover/>
       <Routes>
         <Route path="/" element={<>Hello</>} />
         <Route path="/about" element={<Navbar />} />
---
+++ bash/src/components/combobox.tsx
@@ -0,0 +1,109 @@
+"use client"
+
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+type Status = {
+  value: string
+  label: string
+}
+
+const statuses: Status[] = [
+  {
+    value: "backlog",
+    label: "Backlog",
+  },
+  {
+    value: "todo",
+    label: "Todo",
+  },
+  {
+    value: "in progress",
+    label: "In Progress",
+  },
+  {
+    value: "done",
+    label: "Done",
+  },
+  {
+    value: "canceled",
+    label: "Canceled",
+  },
+]
+
+export function ComboboxPopover() {
+  const [open, setOpen] = React.useState(false)
+  const [selectedStatus, setSelectedStatus] = React.useState<Status | null>(
+    null
+  )
+  const [place,setPlace] = React.useState<string | null>(null)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPlace(e.target.value);
+  };
+
+  return (
+    <div className="flex items-center space-x-4">
+      <p className="text-sm text-muted-foreground">Status</p>
+      {selectedStatus?.value == "canceled" ? 
+      <div className="flex flex-col space-y-4">
+            <input
+             type="text"
+             id="input"
+             onChange={handleChange}
+            placeholder="Enter text"
+    className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+  />
+</div>:
+        <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button variant="outline" className="w-[150px] justify-start">
+            {selectedStatus ? <>{selectedStatus.label}</> : <>+ Set status</>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="p-0" side="right" align="start">
+          <Command>
+            <CommandInput placeholder="Change status..." />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup>
+                {statuses.map((status) => (
+                  <CommandItem
+                    key={status.value}
+                    value={status.value}
+                    onSelect={(value) => {
+                      setSelectedStatus(
+                        statuses.find((priority) => priority.value === value) ||
+                          null
+                      )
+                      setPlace(selectedStatus?.value || null)
+                      setOpen(false)
+                    }}
+                  >
+                    {status.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+      }
+      
+    </div>
+  )
+}
